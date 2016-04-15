fa95cab19d6acf5837de684cbf0c17a7982602dc
warmer <ken.kinion@gmail.com>
2016-04-14 23:11:48 -0400

    Updates the baseline for one of the tests due to refactoring

--- pushfour-website/regress/game_status_happy_path.rb
+++ pushfour-website/regress/game_status_happy_path.rb
@@ -7,13 +7,15 @@ require_relative '../lib/login.rb'
 require_relative '../lib/create_game.rb'
 require_relative '../lib/game_status.rb'
 
+include Pushfour::Website
+
 tcs = [
   { game: {height: 4, width: 4, obstacles: 0, creator: 1, opponent: 2, first_move: 0, user_id: 1},
     moves: [
+      {player: 0, x: 0, y: 0},
       {player: 1, x: 0, y: 0},
-      {player: 2, x: 0, y: 0},
+      {player: 0, x: 0, y: 0},
       {player: 1, x: 0, y: 0},
-      {player: 2, x: 0, y: 0},
     ],
     last_move: 0,
   },
@@ -21,17 +23,17 @@ tcs = [
 seed = 4
 
 Harness.run_test(mock_db: true) do
-  Pushfour::Registration.register(
+  Registration.register(
     name: 'user1',
     password: 'test',
     password2: 'test')
-  Pushfour::Registration.register(
+  Registration.register(
     name: 'user2',
     password: 'test',
     password2: 'test')
 
-  db_result = Pushfour::Database.execute_query <<-HERE
-    SELECT name,passhash,id FROM #{Pushfour::Database::PLAYER_TABLE}
+  db_result = Database.execute_query <<-HERE
+    SELECT name,passhash,id FROM #{Database::PLAYER_TABLE}
   HERE
 
   puts db_result.inspect
@@ -42,7 +44,7 @@ Harness.run_test(mock_db: true) do
     puts "Test case: #{tc.inspect}"
     tc = {rand_seed: seed}.merge(tc)
 
-    create_result = Pushfour::WebGame.create_game(tc[:game])
+    create_result = Pushfour::Website.create_game(tc[:game])
     puts 'Result of game creation:'
     puts create_result.inspect
     game_id = create_result[:game]
@@ -50,14 +52,14 @@ Harness.run_test(mock_db: true) do
     tc[:moves].each_with_index do |move, idx|
       columns = %w(game movenumber player xlocation ylocation)
       values = [game_id, idx, move[:player], move[:x], move[:y]]
-      Pushfour::Database.insert(
-        Pushfour::Database::MOVE_TABLE,
+      Database.insert(
+        Database::MOVE_TABLE,
         columns,
         values
       )
     end
 
-    status = Pushfour::WebGame.get_status(game_id: game_id, last_move: tc[:last_move])
+    status = Pushfour::Website.get_status(game_id: game_id, last_move: tc[:last_move])
     puts status.inspect
 
     puts
